e6e13a6efc91cbc24d742bda874c543c84922ff4
armalite <ttc_rulz@hotmail.com>
2023-01-18 01:38:41 +1300

    Workspace and Client structs

--- rest/prefect.go
+++ rest/prefect.go
@@ -11,6 +11,18 @@ type HTTPTransport struct {
 	Header		http.Header
 }
 
+// Prefect 2 workspace
+type Workspace struct { 
+	Id		string
+	name	string
+}
+
+// Prefect 2 HTTP Client with Workspace included
+type Client struct {
+	HTTPClient http.Client
+	Workspace Workspace
+}
+
 var prefect_api_key = os.Getenv("PREFECT_API_KEY")
 var client = &http.Client{}
 var prefect_base_url = "https://api.prefect.cloud/api/" 
