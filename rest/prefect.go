bdf0f490ee449b1d77a6e111ff0294592a7ac9d9
armalite <ttc_rulz@hotmail.com>
2023-01-18 01:59:49 +1300

    Basic Prefectclient implementation

--- rest/prefect.go
+++ rest/prefect.go
@@ -4,6 +4,8 @@ package rest
 import (
 	"os"
 	"net/http"
+	"fmt"
+	"context"
 )
 
 type HTTPTransport struct {
@@ -14,15 +16,47 @@ type HTTPTransport struct {
 // Prefect 2 workspace
 type Workspace struct { 
 	Id		string
-	name	string
+	Name	string
 }
 
 // Prefect 2 HTTP Client with Workspace included
 type Client struct {
 	HTTPClient http.Client
-	Workspace Workspace
+	//Workspace Workspace
 }
 
 var prefect_api_key = os.Getenv("PREFECT_API_KEY")
 var client = &http.Client{}
 const prefect_base_url = "https://api.prefect.cloud/api/" 
+
+
+// implement the RoundTrip function - needed when generating Prefect Client
+func (tr *HTTPTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	for k, values := range tr.Header {
+		for _, v := range values {
+			req.Header.Add(k, v)
+		}
+	}
+	return tr.RTripper.RoundTrip(req)
+}
+
+
+func PrefectClient(ctx context.Context, apiKey string) (*Client, error) {
+
+	tr := HTTPTransport{
+		RTripper: http.DefaultTransport,
+		Header:       make(http.Header),
+	}
+
+	tr.Header.Add("Authorization", fmt.Sprintf("Bearer %s", *apiKey))
+
+	http_client := http.Client{Transport: &tr}
+
+	// TODO: Get current user
+	// TODO: Get workspace info for current user
+
+	var client Client
+	client.HTTPClient = http_client
+
+	return &client, nil
+}
