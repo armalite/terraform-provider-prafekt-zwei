279bf536d0d2c993d52a8c60252f72a81f86a1df
armalite <ttc_rulz@hotmail.com>
2023-01-18 01:35:54 +1300

    Add HTTPTransport struct

--- rest/prefect.go
+++ rest/prefect.go
@@ -6,6 +6,11 @@ import (
 	"net/http"
 )
 
+type HTTPTransport struct {
+	RTripper 	http.RoundTripper
+	Header		http.Header
+}
+
 var prefect_api_key = os.Getenv("PREFECT_API_KEY")
 var client = &http.Client{}
 var prefect_base_url = "https://api.prefect.cloud/api/" 
