6f869728e511772da20c2b3b2b5bafba0139589d
armalite <ttc_rulz@hotmail.com>
2023-01-18 19:58:54 +1300

    service account CRUD funcs

--- rest/service_accounts.go
+++ rest/service_accounts.go
@@ -28,6 +28,24 @@ type CreateServiceAccountResponse struct {
 	APIKey		ServiceAccountAPIKey
 }
 
+type ReadServiceAccountResponse struct {
+	Id 				string
+	ActorId			string
+	Created			string
+	Updated			string
+	AccountId		string
+	Name 			string
+	AccountRoleName string
+	APIKey			ServiceAccountAPIKey
+}
+
+type DeleteServiceAccountResponse struct {
+	Detail 	string
+}
+
+type UpdateServiceAccountResponse struct {
+	Detail 	string
+}
 
 func CreateServiceAccount(ctx context.Context, client Client, accountid string, name string, api_key_expiration string, account_role_id string, workspace_role_id string) CreateServiceAccountResponse { 
 	post_url := prefect_base_url + "accounts/" + accountid + "/bots/"
@@ -47,6 +65,67 @@ func CreateServiceAccount(ctx context.Context, client Client, accountid string, 
     defer resp.Body.Close()
 
 	var response CreateServiceAccountResponse
+
+    body, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &response)
+
+	return response
+}
+
+
+func ReadServiceAccount(ctx context.Context, client Client, accountid string, botid string) DeleteServiceAccountResponse { 
+	post_url := prefect_base_url + "accounts/" + accountid + "/bots/" + botid
+    req, err := http.NewRequest("GET", post_url, nil)
+    req.Header.Set("Content-Type", "application/json")
+
+    resp, err := client.HTTPClient.Do(req)
+    if err != nil {
+		log.Fatal(err.Error())
+    }
+    defer resp.Body.Close()
+
+	var response DeleteServiceAccountResponse
+
+    body, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &response)
+
+	return response
+}
+
+
+func UpdateServiceAccount(ctx context.Context, client Client, accountid string, botid string, name string) UpdateServiceAccountResponse { 
+	post_url := prefect_base_url + "accounts/" + accountid + "/bots/" + botid
+	var jsonStr = []byte(fmt.Sprintf(`{"name":"%s"}`, name))
+    req, err := http.NewRequest("PATCH", post_url, bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+
+    resp, err := client.HTTPClient.Do(req)
+    if err != nil {
+		log.Fatal(err.Error())
+    }
+    defer resp.Body.Close()
+
+	var response UpdateServiceAccountResponse
+
+    body, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &response)
+
+	return response
+}
+
+
+func DeleteServiceAccount(ctx context.Context, client Client, accountid string, botid string) DeleteServiceAccountResponse { 
+	post_url := prefect_base_url + "accounts/" + accountid + "/bots/" + botid
+    req, err := http.NewRequest("DELETE", post_url, nil)
+    req.Header.Set("Content-Type", "application/json")
+
+    resp, err := client.HTTPClient.Do(req)
+    if err != nil {
+		log.Fatal(err.Error())
+    }
+    defer resp.Body.Close()
+
+	var response DeleteServiceAccountResponse
 
     body, _ := ioutil.ReadAll(resp.Body)
 	json.Unmarshal(body, &response)
